1f6edaeede7566530336087a2ee6a28eecfc1a24
Sergey Lubkov <none@none>
2019-01-10 21:30:45 +0200

    add DBOptionsTest

---
+++ Tests/TestData/TD.Constants.pas
@@ -0,0 +1,14 @@
+﻿unit TD.Constants;
+
+interface
+
+const
+  DBServer = 'localhost';
+  DBPort = 3050;
+  DBDatabase = 'd:\Projects\Delphi\CommonLayer\Tests\DB\Cards.fdb';
+  DBLogin = 'sysdba';
+  DBPassword = 'masterkey';
+
+implementation
+
+end.
