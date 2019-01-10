4bb166b8d2585a566409eab5536c1ff5a38104af
Sergey Lubkov <none@none>
2019-01-10 21:31:20 +0200

    add FBConnection

---
+++ Tests/TestData/TD.FBConnection.pas
@@ -0,0 +1,42 @@
+﻿unit TD.FBConnection;
+
+interface
+
+uses
+  System.Classes, System.SysUtils, System.Variants,App.DBConnection,
+  FireDAC.Comp.Client, IBX.IBServices, FireDAC.Phys.IBWrapper, FireDAC.Phys.IBBase,
+  FireDAC.Phys.FB, App.FBConnection;
+
+type
+  TFBConnection = class(TCLFBConnection)
+  private
+  protected
+    procedure DoConnect(const Connection: TFDConnection); override;
+  public
+    constructor Create(Owner: TComponent); override;
+    destructor Destroy(); override;
+  end;
+
+implementation
+
+{ TFBConnection }
+
+constructor TFBConnection.Create(Owner: TComponent);
+begin
+  inherited;
+
+end;
+
+destructor TFBConnection.Destroy;
+begin
+
+  inherited;
+end;
+
+procedure TFBConnection.DoConnect(const Connection: TFDConnection);
+begin
+  inherited;
+
+end;
+
+end.
