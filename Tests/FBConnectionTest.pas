f3be80d90d6df466180ba0652891a9a9b92b0478
Sergey Lubkov <none@none>
2019-01-10 22:05:33 +0200

    Добавлен тест FBConnectionTest

---
+++ Tests/FBConnectionTest.pas
@@ -0,0 +1,71 @@
+﻿unit FBConnectionTest;
+
+interface
+
+uses
+  TestFramework, System.SysUtils, App.FBConnection, System.Variants,
+  FireDAC.Phys.IBBase, FireDAC.Phys.FB, IBX.IBServices, App.DBConnection,
+  TD.FBConnection, FireDAC.Phys.IBWrapper, System.Classes, FireDAC.Comp.Client,
+  TD.Constants;
+
+type
+  TestTFBConnection = class(TTestCase)
+  strict private
+    FConnection: TFBConnection;
+  public
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    procedure TestConnect;
+    procedure TestSimpleConnect;
+    procedure TestDisconnect;
+  end;
+
+implementation
+
+procedure TestTFBConnection.SetUp;
+begin
+  FConnection := TFBConnection.Create(nil);
+end;
+
+procedure TestTFBConnection.TearDown;
+begin
+  FConnection.Free;
+  FConnection := nil;
+end;
+
+procedure TestTFBConnection.TestConnect;
+begin
+  FConnection.Connect(DBServer, DBDatabase, DBLogin, DBPassword);
+  CheckTrue(FConnection.Connected);
+  CheckTrue(FConnection.ConnectionStatus = cnConnect);
+end;
+
+procedure TestTFBConnection.TestSimpleConnect;
+begin
+  FConnection.Server := DBServer;
+  FConnection.Database := DBDatabase;
+  FConnection.UserName := DBLogin;
+  FConnection.Password := DBPassword;
+
+  FConnection.Connect;
+  CheckTrue(FConnection.Connected);
+  CheckTrue(FConnection.ConnectionStatus = cnConnect);
+end;
+
+procedure TestTFBConnection.TestDisconnect;
+begin
+  FConnection.Connect(DBServer, DBDatabase, DBLogin, DBPassword);
+  CheckTrue(FConnection.Connected);
+  CheckTrue(FConnection.ConnectionStatus = cnConnect);
+
+  FConnection.Disconnect;
+  CheckFalse(FConnection.Connected);
+  CheckTrue(FConnection.ConnectionStatus = cnDisconnect);
+end;
+
+initialization
+  RegisterTest(TestTFBConnection.Suite);
+
+end.
+
