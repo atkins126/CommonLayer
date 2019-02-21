e8e5fc6b7bb8b256678522e60e81d39104ed8c3f
Sergey Lubkov <none@none>
2019-02-21 22:11:26 +0200

    add App.SysUtils

---
+++ Source/App.SysUtils.pas
@@ -0,0 +1,50 @@
+﻿{*******************************************************}
+{                                                       }
+{       Common layer of project                         }
+{                                                       }
+{       Copyright (c) 2018 - 2019 Sergey Lubkov         }
+{                                                       }
+{*******************************************************}
+
+unit App.SysUtils;
+
+interface
+
+uses
+  System.SysUtils, System.Variants;
+
+  {проверяет входной параметр валидность ID}
+  function IsNullID(const Value: Variant): Boolean;
+
+  {Если Value = Null, то возвращаем ReplaceValue}
+  function IsNull(const Value, ReplaceValue: Variant): Variant;
+
+  {Если Value = NullValue, то возвращаем Null}
+  function IfNull(const Value, NullValue: Variant): Variant;
+
+implementation
+
+function IsNullID(const Value: Variant): Boolean;
+begin
+  Result := VarIsEmpty(Value) or VarIsNull(Value) or
+            (VarToStr(Value) = '') or (VarToStr(Value) = '0');
+end;
+
+function IsNull(const Value, ReplaceValue: Variant): Variant;
+begin
+  if Value = Null then
+    Result := ReplaceValue
+  else
+    Result := Value;
+end;
+
+function IfNull(const Value, NullValue: Variant): Variant;
+begin
+  if (Value = NullValue) then
+    Result := Null
+  else
+    Result := Value;
+end;
+
+
+end.
