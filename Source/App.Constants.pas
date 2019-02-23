7dc74923ab400eb627037bbe6eb169016ff1abf8
Sergey Lubkov <none@none>
2019-02-23 21:10:53 +0200

    add App.Constants

---
+++ Source/App.Constants.pas
@@ -0,0 +1,22 @@
+﻿unit App.Constants;
+
+interface
+
+uses
+  Vcl.Forms, System.SysUtils;
+
+var
+  AppPath: string = '';
+  AppName: string = '';
+  AppVersion: string = '1.01';
+  AppMajorVersion: string = '1';
+  AppMinorVersion: string = '01';
+
+implementation
+
+
+initialization
+  AppPath := IncludeTrailingPathDelimiter(ExtractFilePath(Application.ExeName));
+  Delete(AppPath, Length(AppPath), 1);
+
+end.
