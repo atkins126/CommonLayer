4283e23e4693c3e0bd91c1332950aa7c791f52f9
Sergey Lubkov <ser.lubkov@gmail.com>
2021-08-23 18:22:52 +0300

    Добавлены события подключения/отключения БД

--- Source/App.DB.Application.pas
+++ Source/App.DB.Application.pas
@@ -17,6 +17,9 @@ uses
 type
   TCLDBApplication = class(TCLApplication)
   private
+    FAfterConnect: TNotifyEvent;
+    FAfterDisconnect: TNotifyEvent;
+
     procedure ConnectionStatusChanged(Sender: TObject; const Status: TCLConnectionStatus);
     function GetConnected: Boolean;
     procedure SetConnected(const Value: Boolean);
@@ -32,6 +35,8 @@ type
     procedure LoadSettingsFromDB; virtual;
 
     property Connected: Boolean read GetConnected write SetConnected;
+    property AfterConnect: TNotifyEvent read FAfterConnect write FAfterConnect;
+    property AfterDisconnect: TNotifyEvent read FAfterDisconnect write FAfterDisconnect;
   end;
 
 implementation
@@ -42,12 +47,16 @@ constructor TCLDBApplication.Create(Owner: TComponent);
 begin
   inherited;
 
+  FAfterConnect := nil;
+  FAfterDisconnect := nil;
   FDBConnection := DBConnectionClass.Create(Self);
   FDBConnection.OnConnectionStatusChange := ConnectionStatusChanged;
 end;
 
 destructor TCLDBApplication.Destroy;
 begin
+  FAfterConnect := nil;
+  FAfterDisconnect := nil;
 
   inherited;
 end;
@@ -55,8 +64,16 @@ end;
 procedure TCLDBApplication.ConnectionStatusChanged(Sender: TObject;
   const Status: TCLConnectionStatus);
 begin
-  if Status = cnConnect then
-    LoadSettingsFromDB;
+  case Status of
+    cnConnect: begin
+      LoadSettingsFromDB;
+      if Assigned(FAfterConnect) then
+        FAfterConnect(Self);
+    end;
+    else
+      if Assigned(FAfterDisconnect) then
+        FAfterDisconnect(Self);
+  end;
 end;
 
 function TCLDBApplication.GetConnected: Boolean;
